fa4a76b16c3254b61b0f506cc50cd4f7261366b2
RazCrimson <52282402+RazCrimson@users.noreply.github.com>
2023-02-07 01:04:00 +0530

    chg: FastAPI - add version in route paths

--- ango_auth/__main__.py
+++ ango_auth/__main__.py
@@ -6,14 +6,14 @@ from ango_auth.app.routers.auth import auth_router
 
 settings = Settings()
 
-app = FastAPI()
+app = FastAPI(
+    title="Ango - Authentication microservice",
+    docs_url=f"{settings.APP_PREFIX.rstrip('/')}/docs",
+    redoc_url=f"{settings.APP_PREFIX.rstrip('/')}/redoc",
+    openapi_url=f"{settings.APP_PREFIX.rstrip('/')}/openapi.json",
+)
 
+app.include_router(auth_router, prefix=f"{settings.APP_PREFIX.rstrip('/')}/v1", tags=["Auth API V1"])
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
-
-
-app.include_router(auth_router, prefix="/auth")
-
-uvicorn.run(app, host=settings.SERVER_HOST, port=settings.SERVER_PORT)
+if __name__ == "__main__":
+    uvicorn.run(app, host=settings.SERVER_HOST, port=settings.SERVER_PORT)
